bd4d1b68e82c46dfa49de873aee98302da7f8829
Daniel Succar <danielsuccar@icloud.com>
2019-12-19 19:43:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -41,5 +41,5 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
 function destructivelyRemoveElementFromEndOfArray(array){
-  return array.slice(-2)
+  return array.slice(-3)
 }
