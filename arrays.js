46e2cdc9981589e0269d6aef07f9f7d34ec4ec42
Daniel Succar <danielsuccar@icloud.com>
2019-12-19 19:21:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,5 +24,5 @@ function addElementToEndOfArray(array, element){
   return [...array, element]
 }
 function destructivelyAddElementToEndOfArray(array, 'foo'){
-  return [...array, foo]
+  return [...array, 'foo']
 }
