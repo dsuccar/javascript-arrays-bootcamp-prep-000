73ab97fb06933c8939a2ea2e7f455dd6b69c3802
Daniel Succar <danielsuccar@icloud.com>
2019-12-19 19:07:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element){
+  [array,...element]
+}
+
+function addElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
 }
