d0394da1f296d015380bf50b0c5ae691e8004770
Daniel Succar <danielsuccar@icloud.com>
2019-12-19 19:35:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,3 +33,6 @@ function destructivelyAddElementToEndOfArray(array, element){
 function accessElementInArray(array, index){
   return array[index]
 }
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return  array.shift()
+}
